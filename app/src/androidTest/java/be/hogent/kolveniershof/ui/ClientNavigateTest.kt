8fcda0493fe4d7f6569501403ad504d19f8afd7e
Sebastien Wojtyla <sebastien.wojtyla.y3274@student.hogent.be>
2019-12-14 13:52:02 +0100

    Added navigation tests with Wout Maes

--- app/src/androidTest/java/be/hogent/kolveniershof/ui/ClientNavigateTest.kt
+++ app/src/androidTest/java/be/hogent/kolveniershof/ui/ClientNavigateTest.kt
@@ -1,32 +1,31 @@
 package be.hogent.kolveniershof.ui
 
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onData
+import android.app.DatePickerDialog
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers
 import androidx.test.espresso.contrib.NavigationViewActions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.contrib.PickerActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import be.hogent.kolveniershof.R
-import be.hogent.kolveniershof.adapters.DateAdapter
 import junit.framework.Assert.assertEquals
 import org.joda.time.DateTime
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.time.LocalDate
+//import org.robolectric.RobolectricTestRunner
+//import org.robolectric.shadows.ShadowDatePickerDialog
 import java.util.*
 
-@RunWith(AndroidJUnit4::class)
+
+@RunWith(AndroidJUnit4::class/*RobolectricTestRunner::class*/)
 class ClientNavigateTest{
 
     @Rule
@@ -41,26 +40,54 @@ class ClientNavigateTest{
     @Test
     fun swipeToNextDay(){
         //Check if set to current day
-       var date =  rule.activity.findViewById<DateButton>(R.id.dateSelectorNow)
-        assertEquals(date.dateTextView.text,DateTime.now().toString("dd"))
-        assertEquals(date.monthTextView.text,DateTime.now().toString("MMM"))
-
+        checkDay(0)
         //Check ability to swipe to next day
-        //onView(withId(R.id.dateSelectorNow)).check(matches(withText(DateTime.now().toString())))
         onView(withId(R.id.main_content_container)).perform(ViewActions.swipeLeft())
-        date =  rule.activity.findViewById(R.id.dateSelectorNow)
-        assertEquals(date.dateTextView.text,DateTime.now().plusDays(1).toString("dd"))
-        assertEquals(date.monthTextView.text,DateTime.now().plusDays(1).toString("MMM"))
+        //Check if day equals current day +1
+        checkDay(1)
 
+    }
+    @Test
+    fun clickToNextDay(){
+        checkDay(0)
+        onView(withId(R.id.dateSelectorPlusOne)).perform(click())
+        checkDay(1)
+
+    }
+    @Test
+    fun changeDateUsingDatePicker(){
+        /*checkDay(0)
+        onView(withId(R.id.action_calendar)).perform(click())
+        onView(withId(R.id.action_calendar)).perform(PickerActions.setDate(2019,10,28))
+        //onView(withId(R.id.nav_calendar)).check(matches(withText("25-01-2017")));
+        val dialog = ShadowDatePickerDialog.getLatestDialog() as DatePickerDialog
+        val calendar: Calendar = Calendar.getInstance()
+        val expectedYear: Int = calendar.get(Calendar.YEAR)
+        val expectedMonth: Int = calendar.get(Calendar.MONTH)
+        val expectedDay: Int = calendar.get(Calendar.DAY_OF_MONTH)
+
+        assertEquals(dialog.datePicker.dayOfMonth, expectedDay)
+        assertEquals(dialog.datePicker.month, expectedMonth)
+        assertEquals(dialog.datePicker.year, expectedYear)*/
+    }
+    private fun checkDay(i: Int) {
+        val date =  rule.activity.findViewById<DateButton>(R.id.dateSelectorNow)
+        assertEquals(date.dateTextView.text,DateTime.now().plusDays(i).toString("dd"))
+        assertEquals(date.monthTextView.text,DateTime.now().plusDays(i).toString("MMM"))
     }
 
     private fun logout(){
         //Open the menu
-        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
-        onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen()))
+        try {
+            onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+            onView(withId(R.id.drawer_layout)).check(matches(DrawerMatchers.isOpen()))
 
-        onView(withId(R.id.nav_view))
-            .perform(NavigationViewActions.navigateTo(R.id.nav_logout))
+            onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_logout))
+        }catch(e:Exception){
+
+        }
+
     }
 
     private fun login(){
@@ -69,7 +96,7 @@ class ClientNavigateTest{
         onView(withId(R.id.input_password))
             .perform(ViewActions.scrollTo(), ViewActions.typeText("test00##"))
         onView(withId(R.id.button_sign_in))
-            .perform(ViewActions.scrollTo(), ViewActions.click())
+            .perform(ViewActions.scrollTo(), click())
 
 
     }
